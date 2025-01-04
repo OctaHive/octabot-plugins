1892e7513e1566d3eed8fa103009e2d3aeaf9cdc
Roman Efremenko <roman.efremenko@infowatch.com>
2025-01-05 00:12:09 +0300

    chore: change typing and code cleanup

--- crates/gerrit/src/lib.rs
+++ crates/gerrit/src/lib.rs
@@ -1,18 +1,39 @@
 #![allow(deprecated)]
-use std::time::Duration;
+use std::{collections::HashMap, sync::Mutex, time::Duration};
 
 use base64::encode;
-use octabot_rust_sdk::{wit::export, Action, Error, Metadata, Plugin, PluginError};
+use octabot_rust_sdk::{wit::export, ActionData, Error, Metadata, Plugin, PluginError, PluginResult};
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+use strfmt::strfmt;
 use url::Url;
 use waki::{Client, Method, RequestBuilder};
 
 const GERRIT_RESPONSE_PREFIX: &str = ")]}'";
 
+static CONFIG: Lazy<Mutex<Option<Config>>> = Lazy::new(|| Mutex::new(None));
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Config {
+  pub endpoint: String,
+  pub timeout: Option<u64>,
+  pub login: String,
+  pub password: String,
+}
+
 #[derive(Serialize, Deserialize)]
 struct QueryOptions {
   query: String,
   project: String,
+  template: String,
+  review_template: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Params {
+  task_id: String,
+  options: QueryOptions,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -57,26 +78,24 @@ struct Review {
   _number: i32,
 }
 
-#[derive(Serialize, Deserialize)]
-struct GerritPlugin {
-  pub endpoint: String,
-  pub timeout: Option<u64>,
-  pub login: String,
-  pub password: String,
-}
+struct GerritPlugin;
 
 impl GerritPlugin {
-  fn request(&self, method: Method, path: &str) -> Result<RequestBuilder, PluginError> {
-    let url = Url::parse(&format!("https://{}/{}", self.endpoint, path))
-      .map_err(|e| e.to_string())
-      .unwrap();
+  fn request(method: Method, path: &str) -> Result<RequestBuilder, PluginError> {
+    let config = CONFIG
+      .lock()
+      .map_err(|e| PluginError::Other(e.to_string()))?
+      .clone()
+      .ok_or_else(|| PluginError::Other("Config not initialized".to_string()))?;
 
-    let credentials = format!("{}:{}", self.login, self.password);
+    let url = Url::parse(&format!("{}/{}", config.endpoint, path)).map_err(|e| PluginError::Other(e.to_string()))?;
+
+    let credentials = format!("{}:{}", config.login, config.password);
     let authorization = encode(credentials);
 
     let client = Client::new()
       .request(method, url.as_str())
-      .connect_timeout(Duration::from_secs(self.timeout.unwrap_or(60)))
+      .connect_timeout(Duration::from_secs(config.timeout.unwrap_or(60)))
       .headers([
         ("Content-Type", "application/json"),
         ("Authorization", &format!("Basic {}", authorization.as_str())),
@@ -85,41 +104,49 @@ impl GerritPlugin {
     Ok(client)
   }
 
-  fn parse_configuration(config: &str) -> Result<Self, PluginError> {
-    serde_json::from_str::<GerritPlugin>(config)
-      .map_err(|err| PluginError::Other(format!("unable to parse configuration: {}", err)))
+  fn format_review_message(
+    review: &Review,
+    template: &str,
+    config: &Config,
+    params: &QueryOptions,
+  ) -> Result<String, PluginError> {
+    let vars = HashMap::from([
+      ("subject".to_string(), review.subject.clone()),
+      ("insertions".to_string(), review.insertions.to_string()),
+      ("deletions".to_string(), review.deletions.to_string()),
+      ("url".to_string(), config.endpoint.clone()),
+      ("number".to_string(), review._number.to_string()),
+      ("project".to_string(), params.project.to_string()),
+    ]);
+
+    strfmt(template, &vars).map_err(|e| PluginError::Other(format!("Failed to format message: {}", e)))
   }
 }
 
 impl Plugin for GerritPlugin {
-  fn process(config: String, payload: String) -> Result<Vec<Action>, Error> {
-    let query_options = serde_json::from_str::<QueryOptions>(&payload)
-      .map_err(|err| PluginError::Other(format!("unable to parse configuration: {}", err)))?;
+  fn process(payload: String) -> Result<Vec<PluginResult>, Error> {
+    let mut actions = vec![];
+    let params = serde_json::from_str::<Params>(&payload)
+      .map_err(|err| PluginError::ParseActionPaylod(format!("unable to parse gerrit query params: {}", err)))?;
 
-    let query = format!("{} project:{}", query_options.query, query_options.project);
+    let query = format!("{} project:{}", params.options.query, params.options.project);
     let query = [
       ("q", &query),
       ("o", &"DETAILED_ACCOUNTS".to_string()),
       ("o", &"LABELS".to_string()),
     ];
 
-    let gerrit = GerritPlugin::parse_configuration(&config)?;
-    let client = gerrit.request(Method::Get, "a/changes")?.query(&query);
+    let client = GerritPlugin::request(Method::Get, "a/changes")?.query(&query);
 
     let resp = match client.send() {
       Ok(resp) => match resp.status_code() {
         200 => match String::from_utf8(resp.body().unwrap()) {
-          Ok(resp) => Ok(Some(resp)),
-          Err(e) => Err(e.to_string()),
+          Ok(resp) => resp,
+          Err(e) => return Err(PluginError::ParseResponse(e.to_string()).into()),
         },
-        code => Err(format!("HTTP/{}", code)),
+        code => return Err(PluginError::SendHttpRequest(format!("HTTP/{}", code)).into()),
       },
-      Err(e) => Err(e.to_string()),
-    };
-
-    let resp = match resp {
-      Ok(resp) => resp.unwrap_or("".to_owned()),
-      Err(e) => e,
+      Err(e) => return Err(PluginError::SendHttpRequest(e.to_string()).into()),
     };
 
     if resp == "Unauthorized" {
@@ -134,13 +161,50 @@ impl Plugin for GerritPlugin {
       serde_json::from_str(data).map_err(|e| PluginError::Other(format!("Failed to parse response: {}", e)))?;
 
     if !reviews.is_empty() {
-      println!("{:#?}", reviews);
+      let vars = HashMap::from([("project".to_string(), params.options.project.to_string())]);
+      let mut message = strfmt(&params.options.template, &vars)
+        .map_err(|e| PluginError::Other(format!("Failed to format message: {}", e)))?;
+
+      let config = CONFIG
+        .lock()
+        .map_err(|e| PluginError::Other(e.to_string()))?
+        .clone()
+        .ok_or_else(|| PluginError::Other("Config not initialized".to_string()))?;
+
+      for review in reviews {
+        let review_message =
+          GerritPlugin::format_review_message(&review, &params.options.review_template, &config, &params.options)?;
+        message.push_str(&review_message);
+      }
+
+      let action = ActionData {
+        name: "zulip".to_string(),
+        payload: json!({
+          "task_id": params.task_id,
+          "options": {
+            "channel": "Develop_test",
+            "topic": "Подвисшие ревью",
+            "message": message
+          }
+        })
+        .to_string(),
+      };
+
+      actions.push(PluginResult::Action(action));
     }
 
-    Ok(vec![])
+    Ok(actions)
   }
 
-  fn init() -> Metadata {
+  fn init(config: String) -> Result<(), Error> {
+    let config = serde_json::from_str::<Config>(&config).map_err(|err| PluginError::ParseBotConfig(err.to_string()))?;
+    let mut global_config = CONFIG.lock().map_err(|e| PluginError::Other(e.to_string()))?;
+    *global_config = Some(config.clone());
+
+    Ok(())
+  }
+
+  fn load() -> Metadata {
     Metadata {
       name: "Gerrit".to_string(),
       version: "0.1.0".to_string(),
--- crates/zulip/src/lib.rs
+++ crates/zulip/src/lib.rs
@@ -1,17 +1,40 @@
 #![allow(deprecated)]
 use std::time::Duration;
+use std::{num::NonZeroU32, sync::Mutex};
 
 use base64::encode;
-use octabot_rust_sdk::{wit::export, Action, Error, Metadata, Plugin, PluginError};
+use governor::{clock::DefaultClock, state::keyed::DefaultKeyedStateStore, Quota, RateLimiter};
+use octabot_rust_sdk::{wit::export, Error, Metadata, Plugin, PluginError, PluginResult};
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use url::Url;
 use waki::{Client, Method, RequestBuilder};
+
+static RATE_LIMITER: Lazy<Mutex<Option<RateLimiter<String, DefaultKeyedStateStore<String>, DefaultClock>>>> =
+  Lazy::new(|| Mutex::new(None));
+
+static CONFIG: Lazy<Mutex<Option<Config>>> = Lazy::new(|| Mutex::new(None));
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Config {
+  pub endpoint: String,
+  pub timeout: Option<u64>,
+  pub login: String,
+  pub password: String,
+  pub max_request_in_minute: u32,
+}
 
 #[derive(Serialize, Deserialize)]
 struct Message {
   channel: String,
   topic: String,
   message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Params {
+  task_id: String,
+  options: Message,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -21,68 +44,73 @@ struct PostMessageResponse {
   result: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct ZulipPlugin {
-  pub endpoint: String,
-  pub timeout: Option<u64>,
-  pub login: String,
-  pub password: String,
-}
+struct ZulipPlugin;
 
 impl ZulipPlugin {
-  fn request(&self, method: Method, path: &str) -> Result<RequestBuilder, PluginError> {
-    let url = Url::parse(&self.endpoint)
+  fn request(key: String, method: Method, path: &str) -> Result<RequestBuilder, PluginError> {
+    let limiter = RATE_LIMITER.lock().map_err(|e| PluginError::Other(e.to_string()))?;
+    let config = CONFIG
+      .lock()
       .map_err(|e| PluginError::Other(e.to_string()))?
-      .join(path)
-      .map_err(|e| PluginError::Other(e.to_string()))?;
+      .clone()
+      .ok_or_else(|| PluginError::Other("Config not initialized".to_string()))?;
 
-    let credentials = format!("{}:{}", self.login, self.password);
-    let authorization = encode(credentials);
+    if let Some(limiter) = limiter.as_ref() {
+      // Check rate limit
+      if let Err(not_until) = limiter.check_key(&key) {
+        return Err(PluginError::Other(format!(
+          "Rate limit exceeded. Try again in {} seconds",
+          not_until
+            .wait_time_from(governor::clock::Clock::now(limiter.clock()))
+            .as_secs()
+        )));
+      }
 
-    let client = Client::new()
-      .request(method, url.as_str())
-      .connect_timeout(Duration::from_secs(self.timeout.unwrap_or(60)))
-      .headers([
-        ("Content-Type", "application/json"),
-        ("Authorization", &format!("Basic {}", authorization.as_str())),
-      ]);
+      let url = Url::parse(&format!("{}/{}", &config.endpoint, path)).map_err(|e| PluginError::Other(e.to_string()))?;
 
-    Ok(client)
-  }
+      let credentials = format!("{}:{}", config.login, config.password);
+      let authorization = encode(credentials);
 
-  fn parse_configuration(config: &str) -> Result<Self, PluginError> {
-    serde_json::from_str::<ZulipPlugin>(config)
-      .map_err(|err| PluginError::Other(format!("unable to parse configuration: {}", err)))
+      let client = Client::new()
+        .request(method, url.as_str())
+        .connect_timeout(Duration::from_secs(config.timeout.unwrap_or(60)))
+        .headers([
+          ("Content-Type", "application/json"),
+          ("Authorization", &format!("Basic {}", authorization.as_str())),
+        ]);
+
+      Ok(client)
+    } else {
+      return Err(PluginError::Other("Rate limiter not initialized".to_string()));
+    }
   }
 }
 
 impl Plugin for ZulipPlugin {
-  fn process(config: String, payload: String) -> Result<Vec<Action>, Error> {
-    let zulip = ZulipPlugin::parse_configuration(&config)?;
-
-    let message = serde_json::from_str::<Message>(&payload)
-      .map_err(|err| PluginError::Other(format!("unable to parse message: {}", err)))?;
+  fn process(payload: String) -> Result<Vec<PluginResult>, Error> {
+    let params = serde_json::from_str::<Params>(&payload)
+      .map_err(|err| PluginError::ParseActionPaylod(format!("unable to parse zulip message: {}", err)))?;
 
     let query = [
       ("type", "stream"),
-      ("to", &message.channel),
-      ("topic", &message.topic),
-      ("content", &message.message),
+      ("to", &params.options.channel),
+      ("topic", &params.options.topic),
+      ("content", &params.options.message),
     ];
 
-    let client = zulip.request(Method::Post, "api/v1/messages")?.query(&query);
+    let client = ZulipPlugin::request(params.task_id, Method::Post, "api/v1/messages")?.query(&query);
 
     let resp = match client.send() {
       Ok(resp) => match resp.status_code() {
         200 => match String::from_utf8(resp.body().unwrap()) {
           Ok(resp) => {
-            serde_json::from_str::<PostMessageResponse>(&resp).map_err(|e| PluginError::Other(e.to_string()))?
+            serde_json::from_str::<PostMessageResponse>(&resp).map_err(|e| PluginError::ParseResponse(e.to_string()))?
           },
-          Err(e) => return Err(PluginError::Other(e.to_string()).into()),
+          Err(e) => return Err(PluginError::ParseResponse(e.to_string()).into()),
         },
-        code => return Err(PluginError::Other(format!("HTTP/{}", code)).into()),
+        code => return Err(PluginError::SendHttpRequest(format!("HTTP/{}", code)).into()),
       },
-      Err(e) => return Err(PluginError::Other(e.to_string()).into()),
+      Err(e) => return Err(PluginError::SendHttpRequest(e.to_string()).into()),
     };
 
     if resp.result != "success" {
@@ -92,7 +120,28 @@ impl Plugin for ZulipPlugin {
     Ok(vec![])
   }
 
-  fn init() -> Metadata {
+  fn init(config: String) -> Result<(), Error> {
+    let config = serde_json::from_str::<Config>(&config).map_err(|err| PluginError::ParseBotConfig(err.to_string()))?;
+
+    let mut global_config = CONFIG.lock().map_err(|e| PluginError::Other(e.to_string()))?;
+    *global_config = Some(config.clone());
+
+    let mut limiter = RATE_LIMITER.lock().map_err(|e| PluginError::Other(e.to_string()))?;
+    if limiter.is_none() {
+      let rate = NonZeroU32::new(config.max_request_in_minute.max(1))
+        .ok_or_else(|| PluginError::Other("Invalid rate limit value".to_string()))?;
+
+      *limiter = Some(RateLimiter::keyed(Quota::per_minute(rate)));
+      println!(
+        "Rate limiter initialized with {} requests per minute.",
+        config.max_request_in_minute
+      );
+    }
+
+    Ok(())
+  }
+
+  fn load() -> Metadata {
     Metadata {
       name: "Zulip".to_string(),
       version: "0.1.0".to_string(),
